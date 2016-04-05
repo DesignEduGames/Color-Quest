9a59db1f6eb0eeaefd8c301171d2e03c059694f6
Justice Frimpong <jfrimpon@andrew.cmu.edu>
2016-04-05 18:45:34 -0400

    playerController2D and colorPlatform script

---
+++ Color Quest/Assets/Scripts/colorPlatform.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorPlatform : MonoBehaviour {
+
+	private Collider2D myCollider;
+	private SpriteRenderer mySprite;
+	private PlatformEffector2D pe2d;
+	private int playerLayer;
+	private bool playerFallingThrough;
+	private float timer;
+	private float fallTime;
+	public bool regPlat;
+	public bool inactive { get; set; }
+
+	// Use this for initialization
+	void Start () {
+		mySprite = GetComponent<SpriteRenderer> ();
+		playerFallingThrough = false;
+		timer = 0f;
+		fallTime = 0.25f;
+		pe2d = GetComponent<PlatformEffector2D> ();
+		playerLayer = (1 << LayerMask.NameToLayer ("Player"));
+		if (!regPlat) {
+			setInactive ();
+		} else {
+			setActive ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerFallingThrough) {
+			timer += Time.deltaTime;
+			if (timer >= fallTime) {
+				timer = 0f;
+				playerFallingThrough = false;
+				pe2d.colliderMask = pe2d.colliderMask | playerLayer;
+				Debug.Log ("no more fall through");
+			}
+		}
+	}
+
+	public void setActive(){
+		mySprite.color = new Color(mySprite.color.r, mySprite.color.g, mySprite.color.b, 1.0f);
+		pe2d.colliderMask = -1;
+		inactive = false;
+	}
+
+	public void setInactive(){
+		mySprite.color = new Color(mySprite.color.r, mySprite.color.g, mySprite.color.b, 0.2f);
+		pe2d.colliderMask = 0;
+		inactive = true;
+	}
+
+	public void letPlayerFallThrough(){
+		pe2d.colliderMask = pe2d.colliderMask & ~playerLayer; //lets collider ignore player
+		playerFallingThrough = true;
+	}
+
+}
---
+++ Color Quest/Assets/Scripts/playerController_2D.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections.Generic;
+
+public class playerController_2D : MonoBehaviour {
+
+	private Rigidbody2D myRig;
+	private Collider2D myCollider;
+	private SpriteRenderer mySprite;
+	private bool canJump;
+	private bool onPlatform;
+	public float jumpPower;// = 40.0f;
+	public float runSpeed;// = 20.0f;
+	public float maxRunSpeed;// = 10.0f;
+	public float hFirction;// = 0.02f;
+	private float k_GroundedRadius = 0.3f;
+	private Transform m_GroundCheck;
+	private AudioSource myAudio;
+
+	private bool shooting;
+
+	private Dictionary<Color, int> colorToPlatIndex;
+	public GameObject[] coloredPlatforms;
+
+	// Use this for initialization
+	void Start () {
+		myRig = gameObject.GetComponent<Rigidbody2D>();
+		myCollider = gameObject.GetComponent<Collider2D>();
+		mySprite = gameObject.GetComponent<SpriteRenderer>();
+		myAudio = gameObject.GetComponent<AudioSource>();
+		m_GroundCheck = transform.Find("GroundCheck");
+		colorToPlatIndex = new Dictionary<Color, int> ();
+		for (int i = 0; i < coloredPlatforms.Length; i++) {
+			Transform plat = coloredPlatforms [i].transform.GetChild (0);
+			colorToPlatIndex.Add (plat.GetComponent<SpriteRenderer> ().color, i);
+		}
+	}
+
+	void FixedUpdate(){
+		checkShooting ();
+	}
+
+	private void checkShooting(){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		changeSprite ();
+		myRig.velocity = new Vector2 (myRig.velocity.x * hFirction, myRig.velocity.y);
+		checkCanJump ();
+		checkMovement ();
+		checkChangingColor ();
+	}
+
+	private void checkChangingColor(){
+		if (Input.GetKey ("shift")) {
+			//subtractive color mixing, this is 
+		}
+	}
+
+	private void checkMovement(){
+		if (Input.GetKeyDown ("up") || Input.GetKeyDown ("w") || Input.GetKeyDown ("space")) {
+			if (canJump) {
+				onPlatform = false;
+				myRig.velocity = new Vector2 (myRig.velocity.x, jumpPower);
+				canJump = false;
+			}
+		}
+		if (Input.GetKey ("left") || Input.GetKey ("a")) {
+			if (myRig.velocity.x > -maxRunSpeed) {
+				myRig.velocity = new Vector2 (myRig.velocity.x - runSpeed, myRig.velocity.y);
+			}
+			if (myRig.velocity.x <= -maxRunSpeed) {
+				myRig.velocity = new Vector2 (-maxRunSpeed, myRig.velocity.y);
+			}
+		} else if (Input.GetKey ("right") || Input.GetKey ("d")) {
+			if (myRig.velocity.x < maxRunSpeed) {
+				myRig.velocity = new Vector2 (myRig.velocity.x + runSpeed, myRig.velocity.y);
+			}
+			if (myRig.velocity.x >= maxRunSpeed) {
+				myRig.velocity = new Vector2 (maxRunSpeed, myRig.velocity.y);
+			}
+		}
+	}
+
+	private void checkCanJump(){
+		Collider2D[] colliders = Physics2D.OverlapCircleAll (m_GroundCheck.position, 
+			k_GroundedRadius, 1 << LayerMask.NameToLayer ("Platforms"));
+		if (colliders.Length < 1) {
+			canJump = false; //colliding with nothing
+			onPlatform = false;
+		}
+		bool canJumpOnOne = false;
+		for (int i = 0; i < colliders.Length; i++) {
+			try {
+				colorPlatform platScript = colliders [i].GetComponent<colorPlatform> ();
+				if (!platScript.inactive) {
+					canJumpOnOne = true;
+					break;
+				}
+			} catch (Exception e) {
+				if (colliders [i].gameObject != gameObject) {
+					canJumpOnOne = true;
+					break;
+				}
+			}
+		}
+		if (canJumpOnOne) {
+			canJump = true;
+		}
+	}
+
+	private void changeSprite(){
+		//use animator
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "Platforms") {
+			if (!other.gameObject.GetComponent<colorPlatform> ().inactive) {
+				onPlatform = true;
+				canJump = true;
+			}
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D other){
+		if (other.gameObject.tag == "Platforms") {
+			colorPlatform platScript = other.gameObject.GetComponent<colorPlatform> ();
+			if (!platScript.inactive) {
+				if (Input.GetKeyDown ("down") || Input.GetKeyDown ("s")) {
+					platScript.letPlayerFallThrough ();
+				}
+				onPlatform = true;
+			}
+		}
+	}
+		
+}
